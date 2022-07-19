d3ccc5d51be89d12b26d239e2640e45d81a02b56
Ксения Захарова <shadowkksu87@gmail.com>
2022-07-19 20:59:17 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int ticketPrice = 250;
+        int rublesPerBonusMile = 20;
+        int bonusMiles = ticketPrice / rublesPerBonusMile;
+        System.out.println(bonusMiles);
+    }
+}
